22c4156612a56269102135da3a54403a91094e8a
dmkjava <dmk.java@gmail.com>
2021-02-09 00:59:21 +0530

    dsfs

--- src/app/asm.service.ts
+++ src/app/asm.service.ts
@@ -6,7 +6,8 @@ import {HttpClient} from '@angular/common/http'
 })
 export class ASMService{
   products : any;
-  ASM_SERVER_BASE_URL = "http://localhost:3000";
+  ASM_SERVER_BASE_URL = "http://52.15.233.153:3000";
+  // ASM_SERVER_BASE_URL = "http://localhost:3000";
 
   constructor(private http:HttpClient) { }
   getAllContent() {
